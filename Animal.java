8c048fa392e60d14fab8754773adf2f49db98b56
Lee Eisenberg <leeeisenberg05@gmail.com>
2021-10-29 10:10:22 -0400

    classwork

--- Animal.java
+++ Animal.java
@@ -1,14 +1,18 @@
 public class Animal {
-  public String noise;
-  public int age;
+  private String noise;
+  private int age;
   private String name;
-
+//Marie starts
   public Animal(String noise, int age, String name){
     //initialize the values (hint use this.varname to clarify)
+    noise = this.noise;
+    age = this.age;
+    name = this.name;
   }
 
   public String getName() {
     //accessor
+    return name;
   }
 
   public void speak(){
@@ -17,5 +21,8 @@ public class Animal {
     I am ? years old.
     I say ?.
     */
+    System.out.println("My name is " + name);
+    System.out.println("I am " + age + " years old");
+    System.out.println("I say " + noise);
   }
 }
