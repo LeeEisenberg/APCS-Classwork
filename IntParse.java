5823460e636f7e26fc7acd67345cc60ad728d3f6
Lee Eisenberg <leeeisenberg05@gmail.com>
2021-12-21 21:18:26 -0500

    tested and bug fixed

--- IntParse.java
+++ IntParse.java
@@ -5,13 +5,15 @@ public class IntParse{
     boolean negative = false;
     int output = 0;
     int y;
+    int newDigit;
     if (s.charAt(0) == '-'){
       s = s.substring(1);
       negative = true;
     }
     for (int x = 0; x < s.length(); x++){
       y = s.length() - x - 1;
-      output += Math.pow(valueOfDigit(s.charAt(y)), x);
+      newDigit = valueOfDigit(s.charAt(y));
+      output += valueOfDigit(s.charAt(y)) * Math.pow(10, x);
     }
     if (negative){
       return output * -1;
@@ -20,5 +22,12 @@ public class IntParse{
   }
   public static int valueOfDigit(char c){
     return c - 48;
+  }
+
+  public static void main(String[] args){
+    System.out.println(stringToInt("506342") + 1);
+    System.out.println(stringToInt("-450064") + 1);
+    System.out.println(stringToInt("-0000") + 1);
+    System.out.println(stringToInt("004960") + 1);
   }
 }
