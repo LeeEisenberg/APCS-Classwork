5b70859e2d003c7fc2c9fea530419cc9fcd4ff25
Lee Eisenberg <leeeisenberg05@gmail.com>
2022-01-26 10:14:14 -0500

    recursive isEven

--- Recursion.java
+++ Recursion.java
@@ -6,9 +6,22 @@ public class Recursion{
     return 1;
   }
 
+  public static boolean isEven(int n){
+    if (n == 0){
+      return true;
+    }
+    if (n == 1){
+      return false;
+    }
+    return isEven(n-2);
+  }
+
   public static void main(String[] args){
     for (int x = 0; x < 13; x++){
       System.out.println(fact(x));
+    }
+    for (int x = 0; x < 10; x++){
+      System.out.println(isEven(x));
     }
   }
 }
