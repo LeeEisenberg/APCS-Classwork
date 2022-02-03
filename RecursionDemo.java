826ac2de1095cbe279723e05f5bcbb26c0348cf7
Lee Eisenberg <leeeisenberg05@gmail.com>
2022-02-03 00:34:18 -0500

    done with word maker recursion thing

--- RecursionDemo.java
+++ RecursionDemo.java
@@ -1,5 +1,17 @@
 public class RecursionDemo{
-    public static void makeWords(int size, String ans){
-        (makeWords(size-1, ));
+  public static void makeWords(int size, String ans){
+    if (size == 0){
+      System.out.println(ans);
+    }else{
+      for (char x = 'a'; x <= 'z'; x++){
+        makeWords(size-1, ans+x);
+      }
     }
+  }
+
+  public static void main(String[] args){
+    //makeWords(2, "");
+    //makeWords(3, "");
+    makeWords(4, "");
+  }
 }
