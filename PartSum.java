80853e724a9852a4d9c05c1c015d786db8e7e60b
Lee Eisenberg <leeeisenberg05@gmail.com>
2022-02-07 05:13:12 -0500

    base frame of PartSum, with placeholders

---
+++ PartSum.java
@@ -0,0 +1,8 @@
+public class PartSum{
+  public static boolean partSum(int[] nums, int num){
+    return partSum(nums, num, 0);
+  }
+  public static boolean partSum(int[] nums, int num, int index){
+    return false;
+  }
+}
